fbca355154a8a88fa59934d25f3f8fbb57552cc7
Sheldyn Smith <sheldyns@byui.edu>
2019-02-06 09:55:13 -0800

    removed an erroneous error statement

--- web/teamActivities/week05/scriptureSearch.php
+++ web/teamActivities/week05/scriptureSearch.php
@@ -4,8 +4,7 @@
 <body>
     <form name="searchScripture" action="index.php" method="GET">
         Which book do you want to display?<br>
-        <textarea rows="1" cols="40" name="book">
-        </textarea><br>
+        <textarea rows="1" cols="40" name="book"></textarea><br>
         <input type="submit" value="send">
     </form>
 </body>

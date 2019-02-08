c307a9d250f1b7ecf3aa4192f3954f723fd8c964
Sheldyn Smith <sheldyns@byui.edu>
2019-02-07 16:50:57 -0800

    set seach path to correct schema

--- web/rpg/dbConnect.php
+++ web/rpg/dbConnect.php
@@ -17,6 +17,7 @@ function get_db() {
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
 		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		$db->exec("SET search_path TO myschema");
 	}
 	catch (PDOException $ex) {
 		// If this were in production, you would not want to echo

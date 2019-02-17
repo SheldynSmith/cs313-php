a6a6372c36869a5bccc0d4f3319c5854d6185aeb
Sheldyn Smith <sheldyns@byui.edu>
2019-02-16 20:57:50 -0800

    add new users to the database

---
+++ web/rpg/createUser.php
@@ -0,0 +1,20 @@
+<?php
+    require "dbConnect.php";
+
+    $db = get_db();
+    $userName = $_POST["username"];
+    $password = $_POST["password"];
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+    
+    $statement = $db->prepare("INSERT INTO usertable (username, passwordhash)
+                               VALUES :cleanUsername, :cleanPasswordHash");
+    $statement->bindValue(":cleanUsername", $userName, PDO::PARAM_STR);
+    $statement->bindValue(":cleanPasswordHash", $password, PDO::PARAM_STR);
+    $statement->execute();
+
+    $_SESSION["verified"] = FALSE;
+    $_SESSION["username"] = NULL;
+    $_SESSION["retryLogin"] = TRUE;
+    header("Location: login.php");
+    exit;
+?>

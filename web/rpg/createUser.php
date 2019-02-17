b32109dfc7e68eaee2eac73a2834d065dc49dc81
Sheldyn Smith <sheldyns@byui.edu>
2019-02-16 21:08:46 -0800

    replaced an incorrect variable

--- web/rpg/createUser.php
+++ web/rpg/createUser.php
@@ -9,7 +9,7 @@
     $statement = $db->prepare("INSERT INTO usertable (username, passwordhash)
                                VALUES (:cleanUsername, :cleanPasswordHash)");
     $statement->bindValue(":cleanUsername", $userName, PDO::PARAM_STR);
-    $statement->bindValue(":cleanPasswordHash", $password, PDO::PARAM_STR);
+    $statement->bindValue(":cleanPasswordHash", $passwordHash, PDO::PARAM_STR);
     $statement->execute();
 
     $_SESSION["verified"] = FALSE;
